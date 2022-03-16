65fcb529d4d92b5480e692e7f4924553260b8702
huongchun <101695712+huongchun@users.noreply.github.com>
2022-03-16 14:11:22 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "365 ngày của rồi nha aiu",
+    introTitle: 'Dương Lee ',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
+    btnIntro: '^^yêu anhhhh^^',
+    title: 'Có người không để ý đến ngày hôm nay rồi đây mà :)) Đành một mình em nhớ vậy',
+    desc: ' Quên ngày này nhưng em biết anh yêu em nhiều lắm lắm lắm đúng hông? ',
+    btnYes: 'Yêu nhiều lắm luôn <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu em <3',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    reply: 'Yêu thì yêu không yêu thì yêu',
+    mess: 'Em biết mà em cũng yêu anh nhiều lắm đóoooo',
+    messDesc: 'Cùng em đi thêm nhiều nhiều năm nữa nhaaaaa. Yeuanh<',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
